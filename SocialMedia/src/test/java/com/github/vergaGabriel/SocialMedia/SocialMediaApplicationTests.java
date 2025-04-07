20aaa99cce4f5400c5fec80f67897e5ffa416509
vergaGabriel <gabriel_verga@hotmail.com>
2025-04-07 09:14:10 -0300

    Adding project files

---
+++ SocialMedia/src/test/java/com/github/vergaGabriel/SocialMedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vergaGabriel.SocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
